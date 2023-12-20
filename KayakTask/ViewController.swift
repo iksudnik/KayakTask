ed37927286e0d690c3769893ace2c40518dde0d2
Ilya Sudnik <i.sudnik@rubylabs.com>
2023-12-20 13:25:33 +0100

    Initial Commit

---
+++ KayakTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KayakTask
+//
+//  Created by Ilya Sudnik on 20.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
